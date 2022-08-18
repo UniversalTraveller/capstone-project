fbc5965bb7e180dc7d2ebca96512fce1adef0d8f
Andreas Kiener <andreas.kiener@gmail.com>
2022-08-18 10:20:46 +0200

    Remove unnecessary forEach in test

--- src/components/DisplayEpisodes/DisplayEpisodes.spec.js
+++ src/components/DisplayEpisodes/DisplayEpisodes.spec.js
@@ -9,24 +9,23 @@ import DisplayEpisodes from './';
 
 describe('DisplayEpisodes component', () => {
 	it('should display the episodes name', () => {
-		const episodes = [
-			{
-				title: 'CSS Proposals @when, CSS Masonry, Carets',
-				url: 'https://traffic.libsyn.com/syntax/Syntax_-_497.mp3',
-				date: '15.08.2022',
-				length: '00:20:11',
-				key: nanoid(),
-			},
-		];
-		render(<DisplayEpisodes episodes={episodes} />);
+		const episodes = {
+			title: 'CSS Proposals @when, CSS Masonry, Carets',
+			url: 'https://traffic.libsyn.com/syntax/Syntax_-_497.mp3',
+			date: '15.08.2022',
+			length: '00:20:11',
+			key: nanoid(),
+		};
 
-		episodes.forEach(episode => {
-			const episodeTitle = screen.getByText(episode.title);
-			expect(episodeTitle).toBeInTheDocument();
-			const episodeDate = screen.getByText(episode.date);
-			expect(episodeDate).toBeInTheDocument();
-			const episodeLength = screen.getByText(episode.length);
-			expect(episodeLength).toBeInTheDocument();
-		});
+		// The component expects an array as props.
+		const episodesProps = [episodes];
+		render(<DisplayEpisodes episodes={episodesProps} />);
+
+		const episodeTitle = screen.getByText(episodes.title);
+		expect(episodeTitle).toBeInTheDocument();
+		const episodeDate = screen.getByText(episodes.date);
+		expect(episodeDate).toBeInTheDocument();
+		const episodeLength = screen.getByText(episodes.length);
+		expect(episodeLength).toBeInTheDocument();
 	});
 });
