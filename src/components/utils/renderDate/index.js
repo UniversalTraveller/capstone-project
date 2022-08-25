67d454229c39178a944eba7fab29d3b6370fcfaa
Andreas Kiener <andreas.kiener@gmail.com>
2022-08-25 09:56:57 +0200

    Fix renderDate to show correct month

--- src/components/utils/renderDate/index.js
+++ src/components/utils/renderDate/index.js
@@ -1,6 +1,6 @@
 export default function renderDate(dateString) {
 	const date = new Date(dateString);
 	const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
-	const formattedDate = `${day}.${month}.${year}`;
+	const formattedDate = `${day}.${month + 1}.${year}`;
 	return formattedDate;
 }
