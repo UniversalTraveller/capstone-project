ae4583a7e3d62bf8d8b4107ece835f7cf53e0230
Andreas Kiener <andreas.kiener@gmail.com>
2022-08-25 18:00:35 +0200

    Use .toLocaleDateString('de-DE') to render date

--- src/utils/renderDate/index.js
+++ src/utils/renderDate/index.js
@@ -1,6 +1,5 @@
 export default function renderDate(dateString) {
 	const date = new Date(dateString);
-	const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
-	const formattedDate = `${day}.${month + 1}.${year}`;
-	return formattedDate;
+	const formatedDate = date.toLocaleDateString('de-DE');
+	return formatedDate;
 }
