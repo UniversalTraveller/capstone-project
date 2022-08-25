7f7f519dc8f61811f56cb837cb2645e1cb5da44e
Andreas Kiener <andreas.kiener@gmail.com>
2022-08-25 12:15:17 +0200

    Show add podcast button only on relevant pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 
 import {NavigationLink} from '../NavigatonLink/styled';
 
 import {HeaderBar} from './styled';
 
 export default function Header() {
+	const router = useRouter();
+	const path = router.asPath;
 	return (
 		<HeaderBar>
 			<nav>
@@ -14,9 +17,13 @@ export default function Header() {
 				<Link href="/episodes">
 					<NavigationLink>Episodes</NavigationLink>
 				</Link>
-				<Link href="/add-podcast">
-					<NavigationLink>Add</NavigationLink>
-				</Link>
+
+				{/* show add podcast button only when relevant */}
+				{path === '/' || path === '/add-podcast' ? (
+					<Link href="/add-podcast">
+						<NavigationLink>Add</NavigationLink>
+					</Link>
+				) : null}
 			</nav>
 		</HeaderBar>
 	);
