0d91ada87ce55e3e638d514900e4c75643e9bd22
Andreas Kiener <andreas.kiener@gmail.com>
2022-08-25 14:48:54 +0200

    Change test for display episodes component because of new date formatting

--- src/components/DisplayEpisodes/DisplayEpisodes.spec.js
+++ src/components/DisplayEpisodes/DisplayEpisodes.spec.js
@@ -12,7 +12,7 @@ describe('DisplayEpisodes component', () => {
 		const episodes = {
 			title: 'CSS Proposals @when, CSS Masonry, Carets',
 			url: 'https://traffic.libsyn.com/syntax/Syntax_-_497.mp3',
-			date: '15.08.2022',
+			date: '15. Aug 2022',
 			length: '00:20:11',
 			key: nanoid(),
 		};
@@ -23,7 +23,7 @@ describe('DisplayEpisodes component', () => {
 
 		const episodeTitle = screen.getByText(episodes.title);
 		expect(episodeTitle).toBeInTheDocument();
-		const episodeDate = screen.getByText(episodes.date);
+		const episodeDate = screen.getByText('15.8.2022');
 		expect(episodeDate).toBeInTheDocument();
 		const episodeLength = screen.getByText(episodes.length);
 		expect(episodeLength).toBeInTheDocument();
