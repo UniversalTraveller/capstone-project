8399415cdb64118d8b7060b111594c38a2a08c3f
Andreas Kiener <andreas.kiener@gmail.com>
2022-08-29 14:34:46 +0200

    Delete unnecessary code

--- src/components/DeletePodcast/index.js
+++ src/components/DeletePodcast/index.js
@@ -1,5 +1,4 @@
 import {useRouter} from 'next/router';
-import {useState} from 'react';
 
 import useStore from '../../hooks/useStore';
 import {ButtonRow, Notification, NotificationArea} from '../NotificationArea/styled';
@@ -7,7 +6,6 @@ import {ButtonRow, Notification, NotificationArea} from '../NotificationArea/sty
 export default function DeletePodcast() {
 	const router = useRouter();
 	const selectedPodcast = useStore(state => state.selectedPodcast);
-	const [message] = useState('form');
 	const deletePodcast = useStore(state => state.deletePodcast);
 
 	function handleDelete() {
@@ -17,17 +15,13 @@ export default function DeletePodcast() {
 
 	return (
 		<NotificationArea>
-			{message === 'form' ? (
-				<Notification>
-					Do you really want to delete &quot;{selectedPodcast.title}&quot;?
-					<ButtonRow>
-						<button onClick={() => handleDelete()}>Okay</button>
-						<button onClick={() => router.push('/episodes')}>Cancel</button>
-					</ButtonRow>
-				</Notification>
-			) : (
-				''
-			)}
+			<Notification>
+				Do you really want to delete &quot;{selectedPodcast.title}&quot;?
+				<ButtonRow>
+					<button onClick={() => handleDelete()}>Okay</button>
+					<button onClick={() => router.push('/episodes')}>Cancel</button>
+				</ButtonRow>
+			</Notification>
 		</NotificationArea>
 	);
 }
