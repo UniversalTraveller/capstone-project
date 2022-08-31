b69d57542b0fae92e3491275c8835f95804d3572
Andreas Kiener <andreas.kiener@gmail.com>
2022-08-31 12:41:26 +0200

    Show used and unused tags on edit tags page

--- src/pages/edit-tags.js
+++ src/pages/edit-tags.js
@@ -1,10 +1,29 @@
+import {nanoid} from 'nanoid';
+
 import Layout from '../components/Layout';
 import PodcastHeader from '../components/PodcastHeader';
-
+import useStore from '../hooks/useStore';
 export default function EditTags() {
+	const selectedPodcast = useStore(state => state.selectedPodcast);
+	const tags = useStore(state => state.tags);
 	return (
 		<Layout>
 			<PodcastHeader />
+			<ul>
+				{selectedPodcast.tags.map(tag => (
+					<li key={nanoid()}>{tag}</li>
+				))}
+			</ul>
+			<p>
+				Unused Tags:{' '}
+				<ul>
+					{tags.map(tag =>
+						selectedPodcast.tags.find(podcastTag => tag === podcastTag) ? null : (
+							<li key={nanoid()}>{tag}</li>
+						)
+					)}
+				</ul>
+			</p>
 		</Layout>
 	);
 }
