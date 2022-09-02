1ce98d12a779f53ff12087ad0f6df81542996745
Andreas Kiener <andreas.kiener@gmail.com>
2022-09-02 14:36:31 +0200

    Add check for empty tags

--- src/components/CreateTags/index.js
+++ src/components/CreateTags/index.js
@@ -4,17 +4,25 @@ import {Form, ButtonRow} from '../NotificationArea/styled';
 export default function CreateTags({selectedPodcast}) {
 	const addTagToPodcast = useStore(state => state.addTagToPodcast);
 	const addTag = useStore(state => state.addTag);
+	const tags = useStore(state => state.tags);
 	function handleSubmit(event) {
 		event.preventDefault();
 		const input = event.target.tags.value;
-		const tags = input.split(',').map(tag => tag.trim());
-		// add tags to podcast and to tags array
-		tags.forEach(tag => {
-			addTagToPodcast(tag, selectedPodcast);
-			addTag(tag);
-		});
 
-		console.log('input', tags);
+		// avoid creation of empty tags
+		if (input.length === 0) {
+			return;
+		}
+
+		const newTags = input.split(',').map(tag => tag.trim());
+
+		newTags.forEach(tag => {
+			// check if the tag already exists
+			if (!selectedPodcast.tags.includes(tag) && !tags.includes(tag)) {
+				addTagToPodcast(tag, selectedPodcast);
+				addTag(tag);
+			}
+		});
 	}
 
 	return (
@@ -23,12 +31,7 @@ export default function CreateTags({selectedPodcast}) {
 				<label htmlFor="tags">Create new tags:</label>
 			</ButtonRow>
 			<ButtonRow>
-				<input
-					type="text"
-					name="tags"
-					id="tags"
-					placeholder="tag, another tag, another awesome tag"
-				/>
+				<input type="text" name="tags" id="tags" placeholder="tag, tag, tag, ..." />
 
 				<button type="submit">Submit</button>
 			</ButtonRow>
