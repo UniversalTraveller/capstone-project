834d104eb24377b101e613ac91eeaf9203ac648f
Andreas Kiener <andreas.kiener@gmail.com>
2022-09-02 15:44:39 +0200

    Add message when duplicate tags are entered

--- src/components/CreateTags/index.js
+++ src/components/CreateTags/index.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 import useStore from '../../hooks/useStore';
 import {Form, ButtonRow} from '../NotificationArea/styled';
 
@@ -5,8 +7,15 @@ export default function CreateTags({selectedPodcast}) {
 	const addTagToPodcast = useStore(state => state.addTagToPodcast);
 	const addTag = useStore(state => state.addTag);
 	const tags = useStore(state => state.tags);
+	let [duplicateTags, setDuplicateTags] = useState([]);
+
 	function handleSubmit(event) {
 		event.preventDefault();
+
+		// reset duplicateTags array every time the form is submitted
+		duplicateTags = [];
+		setDuplicateTags(duplicateTags);
+
 		const input = event.target.tags.value;
 
 		// avoid creation of empty tags
@@ -21,6 +30,10 @@ export default function CreateTags({selectedPodcast}) {
 			if (!selectedPodcast.tags.includes(tag) && !tags.includes(tag)) {
 				addTagToPodcast(tag, selectedPodcast);
 				addTag(tag);
+			} else {
+				duplicateTags.push(tag);
+				setDuplicateTags(duplicateTags);
+				console.log(duplicateTags);
 			}
 		});
 	}
@@ -35,6 +48,9 @@ export default function CreateTags({selectedPodcast}) {
 
 				<button type="submit">Submit</button>
 			</ButtonRow>
+			{duplicateTags.length ? (
+				<ButtonRow>The following tags already exist: {duplicateTags.join(', ')}</ButtonRow>
+			) : null}
 		</Form>
 	);
 }
