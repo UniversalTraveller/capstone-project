b2a7a6b5a8c319c6da02ee3ec6415849371263f9
Andreas Kiener <andreas.kiener@gmail.com>
2022-09-03 20:18:28 +0200

    Fix tests for CreatTags

--- src/components/CreateTags/CreateTags.spec.js
+++ src/components/CreateTags/CreateTags.spec.js
@@ -13,17 +13,42 @@ describe('CreateTags', () => {
 	it('should create a new tag', async () => {
 		const store = renderHook(() => useStore());
 		const {addTagToPodcast} = store.result.current;
+		const {addTag} = store.result.current;
 
 		const selectedPodcast = store.result.current.podcasts[0];
 
 		render(<CreateTags selectedPodcast={selectedPodcast} />);
-		const submitButton = find('Submit');
-		//const theForm = screen.getByTitle('newTags');
-		const inputElement = screen.getByRole('textbox', {name: 'Create new tags:'});
-		await userEvent.type(inputElement, 'tagalong');
-		//await userEvent.submit(inputElement);
-		submitButton.simulate('click');
-		expect(addTagToPodcast).toHaveBeenCalled();
-		//expect(inputElement).toHaveValue('tag3');
+
+		const submitButton = screen.getByRole('button', {name: /Submit/i});
+		const tagsInput = screen.getByLabelText(/Create new tags:/i);
+
+		await userEvent.type(tagsInput, 'new tag');
+		await userEvent.click(submitButton);
+
+		expect(addTagToPodcast).toHaveBeenCalledTimes(1);
+		expect(addTagToPodcast).toHaveBeenCalledWith('new tag', selectedPodcast);
+		expect(addTag).toHaveBeenCalledTimes(1);
+		expect(addTag).toHaveBeenCalledWith('new tag');
+	});
+});
+
+describe('CreateTags', () => {
+	it('should not create a new tag when calld with an already existing tag', async () => {
+		const store = renderHook(() => useStore());
+		const {addTagToPodcast} = store.result.current;
+		const {addTag} = store.result.current;
+
+		const selectedPodcast = store.result.current.podcasts[0];
+
+		render(<CreateTags selectedPodcast={selectedPodcast} />);
+
+		const submitButton = screen.getByRole('button', {name: /Submit/i});
+		const tagsInput = screen.getByLabelText(/Create new tags:/i);
+
+		await userEvent.type(tagsInput, 'tech');
+		await userEvent.click(submitButton);
+
+		expect(addTagToPodcast).toHaveBeenCalledTimes(0);
+		expect(addTag).toHaveBeenCalledTimes(0);
 	});
 });
