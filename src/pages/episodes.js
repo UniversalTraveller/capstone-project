b0efe6609aa49a72389b863351b1430297fc4917
Andreas Kiener <andreas.kiener@gmail.com>
2022-09-05 18:43:45 +0200

    Add tags to podcast details view

--- src/pages/episodes.js
+++ src/pages/episodes.js
@@ -1,11 +1,20 @@
 import DisplayEpisodes from '../components/DisplayEpisodes';
+import {TagList, Tag} from '../components/EditTags/styled';
 import Layout from '../components/Layout';
 import PodcastHeader from '../components/PodcastHeader';
+import useStore from '../hooks/useStore';
 
 export default function Episodes() {
+	const {selectedPodcast} = useStore(state => state);
 	return (
 		<Layout>
 			<PodcastHeader />
+
+			<TagList>
+				{selectedPodcast.tags.map(tag => (
+					<Tag key={tag}>{tag}</Tag>
+				))}
+			</TagList>
 			<DisplayEpisodes />
 		</Layout>
 	);
