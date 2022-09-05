206308663fcc980aebfbe0054830680f034d660e
Andreas Kiener <andreas.kiener@gmail.com>
2022-09-05 15:07:04 +0200

    Add PodcastHeader to delete podcast page

--- src/pages/delete-podcast.js
+++ src/pages/delete-podcast.js
@@ -1,16 +1,15 @@
 import DeletePodcast from '../components/DeletePodcast';
 import DisplayEpisodes from '../components/DisplayEpisodes';
 import Layout from '../components/Layout';
-import useStore from '../hooks/useStore';
+import PodcastHeader from '../components/PodcastHeader';
 
 export default function DeletePodcastPage() {
-	const selectedEpisodes = useStore(state => state.selectedEpisodes);
-
 	return (
 		<>
 			<DeletePodcast />
 			<Layout>
-				<DisplayEpisodes episodes={selectedEpisodes} />
+				<PodcastHeader />
+				<DisplayEpisodes />
 			</Layout>
 		</>
 	);
