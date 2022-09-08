5359a32d31fb6cf2094b5280242bfe2c25dde10d
Andreas Kiener <andreas.kiener@gmail.com>
2022-09-08 15:41:34 +0200

    Add tags to delete podcast page

--- src/pages/delete-podcast/[podcast].js
+++ src/pages/delete-podcast/[podcast].js
@@ -1,14 +1,24 @@
+import {useRouter} from 'next/router';
+
 import DeletePodcast from '../../components/DeletePodcast';
 import DisplayEpisodes from '../../components/DisplayEpisodes';
+import {TagList, Tag} from '../../components/EditTags/styled';
 import Layout from '../../components/Layout';
 import PodcastHeader from '../../components/PodcastHeader';
+import useStore from '../../hooks/useStore';
 
 export default function DeletePodcastPage() {
+	const router = useRouter();
+	const {id} = router.query;
+	const podcast = useStore(state => state.podcasts.find(podcast => podcast.key === id));
 	return (
 		<>
 			<DeletePodcast />
 			<Layout>
 				<PodcastHeader />
+				<TagList>
+					{podcast ? podcast.tags.map(tag => <Tag key={tag}>{tag}</Tag>) : null}
+				</TagList>
 				<DisplayEpisodes />
 			</Layout>
 		</>
